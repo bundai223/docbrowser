f8e7d1f19497e8d12c27c9519a775b5eb1d6b756
Daiji NISHIMURA <bundai223@gmail.com>
2023-06-14 09:40:48 +0000

    update devcontainer

---
+++ .devcontainer/postCreated.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+echo This is postCreate Script!
+
+# cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+# cpanm JSON
+# cpanm Net::SSLeay
+# cpanm LWP::Simple LWP::Protocol::https
