d7b2e803490f91a4577f0da8259763a2508cdb7e
Daiji NISHIMURA <bundai223@gmail.com>
2023-06-23 18:06:26 +0900

    eslintとprettier実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-// import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+// import React, { useState } from "react";
+// import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { registerAll } from '@tauri-apps/api/globalShortcut';
 
