0d95bff43fdfa86a1bd67eb640b6c8272c58f9f6
Daiji NISHIMURA <bundai223@gmail.com>
2023-07-03 10:15:17 +0000

    cpanm

---
+++ .devcontainer/postStart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+
+echo This is postStart Script!
+
+# cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+# cpanm JSON Net::SSLeay LWP::Simple LWP::Protocol::https
