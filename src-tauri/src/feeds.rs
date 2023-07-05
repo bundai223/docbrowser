6343d45bc1b6b3cdfeffafe19cd7fdf4d3fe63fb
Daiji NISHIMURA <bundai223@gmail.com>
2023-07-05 17:27:24 +0000

    prepare test data file.

--- src-tauri/src/feeds.rs
+++ src-tauri/src/feeds.rs
@@ -11,28 +11,56 @@ fn read_feed<R: std::io::Read>(bufreader: BufReader<R>) {
     let mut depth = 0;
     for e in parser {
         match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{:spaces$}+{name}", "", spaces = depth * 2);
-                depth += 1;
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
-                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+            Ok(e) => {
+                match e {
+                    XmlEvent::StartDocument { version, encoding, .. } => {
+                        println!("StartDocument({version}, {encoding})")
+                    },
+                    XmlEvent::EndDocument => {
+                        println!("EndDocument");
+                        break;
+                    }
+                    XmlEvent::ProcessingInstruction { name, data } => {
+                        println!("ProcessingInstruction({name}={:?})", data.as_deref().unwrap_or_default())
+                    },
+                    XmlEvent::StartElement { name, attributes, .. } => {
+                        if attributes.is_empty() {
+                            println!("StartElement({name})")
+                        } else {
+                            let attrs: Vec<_> = attributes
+                                .iter()
+                                .map(|a| format!("{}={:?}", &a.name, a.value))
+                                .collect();
+                            println!("StartElement({name} [{}])", attrs.join(", "))
+                        }
+                    }
+                    XmlEvent::EndElement { name } => {
+                        println!("EndElement({name})")
+                    },
+                    XmlEvent::Comment(data) => {
+                        println!(r#"Comment("{}")"#, data.escape_debug())
+                    }
+                    XmlEvent::CData(data) => println!(r#"CData("{}")"#, data.escape_debug()),
+                    XmlEvent::Characters(data) => {
+                        println!(r#"Characters("{}")"#, data.escape_debug())
+                    }
+                    XmlEvent::Whitespace(data) => {
+                        println!(r#"Whitespace("{}")"#, data.escape_debug())
+                    }
+                }
             }
             Err(e) => {
                 eprintln!("Error: {e}");
                 break;
-            }
-            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
-            _ => {}
+            },
         }
     }
     println!("Length: {}", depth);
 }
 
 // async fn download_feed() -> Result<(), reqwest::Error> {
-async fn download_feed() -> Result<String, reqwest::Error> {
-    let content = reqwest::get("https://raw.githubusercontent.com/Kapeli/feeds/master/Rust.xml")
+async fn download_feed(url: &str) -> Result<String, reqwest::Error> {
+    let content = reqwest::get(url)
         .await?
         .text()
         .await?;
@@ -47,7 +75,7 @@ mod tests {
 
     #[test]
     fn test_read_feed() {
-        let file = match File::open("Rust.xml") {
+        let file = match File::open("./spec/Rust.xml") {
             Ok(it) => it,
             Err(why) => panic!("couldn't open {}: {}", "Rust.xml", why),
         };
@@ -57,7 +85,7 @@ mod tests {
 
     #[actix_rt::test]
     async fn test_buf_feed() {
-        let content = match download_feed().await {
+        let content = match download_feed("https://raw.githubusercontent.com/Kapeli/feeds/master/Rust.xml").await {
             Ok(it) => it,
             Err(why) => panic!("download {}: {}", "aaa", why),
         };
