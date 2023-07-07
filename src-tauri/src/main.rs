2c7623fb5dfb9ab4bb269a89f3d51c39b2cb82ec
Daiji NISHIMURA <bundai223@gmail.com>
2023-07-07 18:52:48 +0900

    rust側とのやりとり

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,14 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn docsets() -> [&'static str; 2] {
+    ["docset a", "docset b" ]
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, docsets])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,6 @@
 import "./Sidebar.css";
+import { invoke } from '@tauri-apps/api/tauri'
+import { useState } from "react";
 
 type Item = {
   name: string
@@ -7,10 +9,20 @@ type Item = {
 
 function Sidebar() {
 
-  const items: Item[] = [
+  const [items, setItems] = useState<Item[]>([
     { name: 'aaaa', link: '' },
     { name: 'bbbb', link: '' }
-  ]
+  ])
+
+  invoke('docsets').then((docsets) => {
+    console.log(docsets)
+
+    if(docsets instanceof Array<string>) {
+      setItems(
+        docsets.map((name: string) => { return { name: name, link: '' } })
+      )
+    }
+  })
 
   const list = items.map((item) =>
     <li key={item.name}>
