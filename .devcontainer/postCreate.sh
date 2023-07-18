b88305edf77dcc1b5055b07b0d05ce816ea71caf
Daiji NISHIMURA <bundai223@gmail.com>
2023-07-18 14:55:52 +0000

    Dockerfileでやることできるだけ減らすテスト

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,17 @@ set -eu
 
 echo This is postCreate Script!
 
-# cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
-# cpanm JSON
-# cpanm Net::SSLeay
-# cpanm LWP::Simple LWP::Protocol::https
+sudo mkdir -p /workspaces/docbrowser
+sudo chown -R 1000:1000 /workspaces
+mkdir -p /workspaces/docbrowser/node_modules
+
+sudo chmod -R g+w /usr/local/cargo /usr/local/rustup
+sudo chown -R :rustlang /usr/local/cargo
+sudo chown -R :rustlang /usr/local/rustup
+
+# cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib) \
+# cpanm JSON Net::SSLeay LWP::Simple LWP::Protocol::https
+
+cargo install tauri-cli@1.4.0
+
+pnpm i
