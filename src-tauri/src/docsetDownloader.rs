65c8fd1536729eb05ee9eb791ae37925aeb171bd
Daiji NISHIMURA <bundai223@gmail.com>
2023-10-02 19:32:25 +0900

    docsetダウンロード

--- src-tauri/src/docsetDownloader.rs
+++ src-tauri/src/docsetDownloader.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use std::fs::File;
 use std::io::copy;
 use std::path::Path;
@@ -5,12 +7,14 @@ use tar::Archive;
 use flate2::read::GzDecoder;
 
 pub fn download_and_extract(url: &str, dest: &Path) {
-    match download_file(url, dest) {
+    let tmp_work = Path::new("./spec/tmp/");
+    let tmp_file = Path::new("./spec/tmp/tmp.tgz");
+    match download_file(url, tmp_file) {
         Ok(()) => println!("downloaded"),
         Err(why) => panic!("download {}: {}", url, why),
     };
 
-    extract(dest, Path::new("./extract"));
+    extract(tmp_file, tmp_work);
 }
 
 pub fn download_file(url: &str, dest: &Path) -> Result<(), Box<dyn std::error::Error>>{
@@ -25,7 +29,12 @@ pub fn extract(tgz_path: &Path, dest: &Path) -> Result<(), Box<dyn std::error::E
     let tar_gz = File::open(tgz_path)?;
     let tar = GzDecoder::new(tar_gz);
     let mut archive = Archive::new(tar);
-    archive.unpack(dest.parent().unwrap())?;
+
+    if !dest.exists() {
+        fs::create_dir_all(dest)?;
+    }
+
+    archive.unpack(dest)?;
 
     Ok(())
 }
@@ -38,7 +47,7 @@ mod tests {
     #[test]
     fn test_download_docset() {
         let url = "http://sanfrancisco.kapeli.com/feeds/Rust.tgz";
-        let dest = Path::new("Rust.tgz");
+        let dest = Path::new("./spec/tmp/extract/");
         download_and_extract(url, &dest);
     }
 }
