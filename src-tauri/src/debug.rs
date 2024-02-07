0de4c508cfbc92f35f368096731c78a6117b5ebb
Daiji NISHIMURA <bundai223@gmail.com>
2024-02-07 04:24:15 +0000

    昼休みの差分あげとく

--- src-tauri/src/debug.rs
+++ src-tauri/src/debug.rs
@@ -1,8 +1,12 @@
 use std::env;
+use tauri::App;
 
-pub fn debug_print() -> std::io::Result<()> {
-    let path = env::current_dir()?;
-    println!("starting dir: {}", path.display());
+pub fn debug_print(app: &App) -> std::io::Result<()> {
+    println!("env::current_dir: {}", env::current_dir()?.display());
+    println!("app_local_data_dir: {}", app.path_resolver().app_local_data_dir().unwrap().display());
+    println!("app_data_dir: {}", app.path_resolver().app_data_dir().unwrap().display());
+    println!("app_config_dir: {}", app.path_resolver().app_config_dir().unwrap().display());
+    println!("app_cache_dir: {}", app.path_resolver().app_cache_dir().unwrap().display());
 
     Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 mod router;
 mod feeds;
 mod docset;
@@ -9,9 +11,11 @@ mod debug;
 
 #[tokio::main]
 async fn main() {
-    let _ = debug::debug_print();
-
     tauri::Builder::default()
+        .setup(|app| {
+            let _ = debug::debug_print(&app);
+            Ok(())
+        })
         .plugin(rspc::integrations::tauri::plugin(router::mount(), || ()))
         // .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
